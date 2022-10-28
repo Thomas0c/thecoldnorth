26c7f96654d93cdada8b47334ca11f0592d26ebf
Thomas0c <8699937+Thomas0c@users.noreply.github.com>
2022-10-28 21:36:02 +0200

    feat(layout): Add page and layout structure & initial content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,26 @@
 import "../styles/globals.css";
-import { Inter_Tight } from "@next/font/google";
+import { Inter } from "@next/font/google";
 
 // If loading a variable font, you don't need to specify the font weight
-const interTight = Inter_Tight();
+const inter = Inter({ subsets: ["latin"], preload: true });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en" className={interTight.className}>
+		<html lang="en" className={inter.className}>
 			<head>
 				<title>The Cold North</title>
-        <link href="/favicon.png"  type="image/png" rel="icon" media="(prefers-color-scheme: light)" />
-				<link href="/favicon-dark.png"  type="image/png" rel="icon" media="(prefers-color-scheme: dark)" />
+				<link
+					href="/favicon.png"
+					type="image/png"
+					rel="icon"
+					media="(prefers-color-scheme: light)"
+				/>
+				<link
+					href="/favicon-dark.png"
+					type="image/png"
+					rel="icon"
+					media="(prefers-color-scheme: dark)"
+				/>
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
@@ -27,7 +37,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<meta name="theme-color" media="(prefers-color-scheme: light)" content="#F3EFE0" />
 				<meta name="theme-color" media="(prefers-color-scheme: light)" content="#30343F" />
 			</head>
-			<body>{children}</body>
+			<body className="bg-gray-dark">{children}</body>
 		</html>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,119 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-eggshell dark:bg-gray">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className="mx-auto max-w-[700px] px-8 pt-16 leading-relaxed antialiased md:pt-44">
+			<main className="mb-12">
+				<h1 className="text-2xl text-white font-semibold text-neutral-50">
+					The Cold North
+				</h1>
+				<h2 className="text-coral font-bold">Thomas Carlson</h2>
+				<p className="mt-6 text-xanadu">
+					Full-stack developer based out of Copenhagen, Denmark. Helping companies
+					architect and build <span className="text-white">apps</span> and{" "}
+					<span className="text-white">websites</span>. Happy working with{" "}
+					<span className="text-white">TypeScript</span>,{" "}
+					<span className="text-white">React Native</span>,{" "}
+					<span className="text-white">Next.js</span> &amp;{" "}
+					<span className="text-white">GraphQL</span>. Huge fan of <span className="text-white">case study houses</span>.
+				</p>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+				<p className="mt-6 text-xanadu">
+					Check out{" "}
+					<a
+						href="https://github.com/thomas0c"
+						className="border-b border-xanadu pb-px transition-colors hover:border-white hover:text-white hover:duration-300"
+					>
+						GitHub
+					</a>
+					, drop by{" "}
+					<a
+						href="https://github.com/thomas0c"
+						className="border-b border-xanadu pb-px transition-colors hover:border-white hover:text-white hover:duration-300"
+					>
+						LinkedIn
+					</a>
+					.
+				</p>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+				<div className="mt-14 grid grid-cols-12 gap-y-4 md:gap-x-8">
+					<div className="col-span-full md:col-span-4 sticky">
+						<h2 className="font-semibold text-white">Experience &amp; projects</h2>
+					</div>
+					<div className="col-span-full md:col-span-8 md:col-start-5 space-y-4 text-xanadu">
+						<div className="group">
+							<h2 className="font-semibold transition-colors group-hover:text-coral hover:duration-300">
+								Tech Lead at NoA Ignite Denmark
+							</h2>
+							<p className="text-xs transition-colors group-hover:text-eggshell hover:duration-100">
+								2019-current
+							</p>
+							<p className="text-xs mt-2 transition-colors group-hover:text-white hover:duration-300 leading-5">
+								Tech lead and full-stack developer building apps, e-commerce
+								websites and single page applications with tools such as Next.js,
+								Vercel, Sanity, React Native and Node.js. Responsible for
+								architecting, planning and building digital experiences.
+							</p>
+							<p className="text-xs transition-colors group-hover:text-white hover:duration-300 mt-2 leading-5">
+								Won gold at Danish Digital Awards within{" "}
+								<span className="text-white">innovation</span> and{" "}
+								<span className="text-white">data science</span> for WARD 24/7.{" "}
+							</p>
+						</div>
+						<div className="group pt-4">
+							<h2 className="font-semibold transition-colors group-hover:text-coral hover:duration-300 leading-5">
+								Developer at ustwo Malmö
+							</h2>
+							<p className="text-xs transition-colors group-hover:text-eggshell hover:duration-100">
+								2018-2019
+							</p>
+							<p className="text-xs mt-2 transition-colors group-hover:text-white hover:duration-300 leading-5">
+								App & web developer working with clients such as Save the Children
+								Sweden enabling best-in-class experiences with technologies like
+								React Native, Next.js and Node.js.
+							</p>
+							<p className="text-xs mt-2 transition-colors group-hover:text-white hover:duration-300 leading-5">
+								Safe Place app was awarded in the{" "}
+								<span className="text-white">
+									Fast Company Innovation by Design
+								</span>{" "}
+								Awards in 2019 in the Health and Social Good category.
+							</p>
+						</div>
+						<div className="group pt-4">
+							<h2 className="font-semibold transition-colors group-hover:text-coral hover:duration-300">
+								Engineering Lead at Siberia New York
+							</h2>
+							<p className="text-xs transition-colors group-hover:text-eggshell hover:duration-300 transition-colors group-hover:text-white hover:duration-100">
+								2014-2017
+							</p>
+							<p className="text-xs mt-2 transition-colors group-hover:text-white hover:duration-300 leading-5">
+								Started as an intern. Ended up as an engineering lead. Built
+								chatbots, websites, prototypes, native apps. Also did technical
+								strategic research and innovation outlooks. Fortunate enough to work with brands such as Avis, Google, Nike and Intel. 
+							</p>
+						</div>
+						<div className="group pt-4">
+							<h2 className="font-semibold transition-colors group-hover:text-coral hover:duration-300">
+								Creative Technologist at Another Copenhagen
+							</h2>
+							<p className="text-xs transition-colors group-hover:text-eggshell hover:duration-300 transition-colors group-hover:text-white hover:duration-100">
+								2013-2014
+							</p>
+							<p className="text-xs mt-2 transition-colors group-hover:text-white hover:duration-300 leading-5">
+								Worked as creative technologist helping establish brand new agency
+								Another when established in August 2013. Responsible for all digital
+								products and projects including campaigns for SELECTED, Birger1962,
+								Wood Wood and BØRNEfonden.
+							</p>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
+};
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-      </footer>
-    </div>
-  )
-}
-
-export default Home
+export default Home;
